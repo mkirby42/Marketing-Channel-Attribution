5b4338ebcbcdf52b13c68656621789c433ab36a4
Matt.Kirby <mkirby3@angelo.edu>
2019-10-24 16:26:19 -0500

    hello

--- Markov Chain Attribution Modeling.R
+++ Markov Chain Attribution Modeling.R
@@ -1,7 +1,5 @@
 #! /usr/bin/Rscript
 
-install.packages("ChannelAttribution",repos = "http://cran.us.r-project.org")
-
 # Read in libraries
 if (!require(ChannelAttribution)){
 install.packages("ChannelAttribution")
@@ -14,12 +12,39 @@ setwd <- setwd('/Users/mattkirby/repos/Marketing-Channel-Attribution')
 # Read in csv
 df <- read.csv('Paths.csv')
 
-df <- df[c(1,2)]
+df <- df[c(1,2,3)]
 
-#Run Markov Model
-M <- markov_model(df, 'Path', var_value = 'Conversion', var_conv = 'Conversion', sep = '>', order=1, out_more = TRUE)
+# Run Markov Model
+M <- markov_model(Data = df,
+                  var_path = 'Path',
+                  var_conv = 'Conversion',
+                  var_value = 'Conversion_value',      
+                  sep = '>',
+                  order = 1,
+                  out_more = TRUE)
 
 write.csv(M$result, file = "Markov - Output.csv", row.names = FALSE)
 
 # Output transition matrix as well
 write.csv(M$transition_matrix, file = "Markov - Output - Transition Matrix.csv", row.names = FALSE)
+
+
+# Description
+#    Estimate a k-order Markov model from customer journey data.
+# Usage
+#    markov_model(Data, var_path, var_conv, var_value=NULL, var_null=NULL,
+#    order=1, nsim=NULL, max_step=NULL, out_more=FALSE, sep=">",
+#    seed=NULL)
+# Arguments
+#    Data data.frame containing paths and conversions.
+#    var_path column name containing paths.
+#    var_conv column name containing total conversions.
+#    var_value column name containing total conversion value.
+#    var_null column name containing total paths that do not lead to conversions.
+#    order Markov Model order.
+#    nsim total simulations from transition matrix.
+#    max_step maximum number of steps for a single simulated path.
+#    out_more if TRUE returns the transition probabilities between channels and removal effects.
+#    sep separator between the channels.
+#    seed random seed. Giving to this parameter the same value over different runs guarantee that results will
+#    not vary.
--- markov_model.py
+++ markov_model.py
@@ -1,7 +1,45 @@
 import subprocess
+import pandas as pd
+import numpy as np
 
-path2script = "Markov Chain Attribution Modeling.R"
 
-cmd = ['Rscript', path2script]
+def process_data():
 
-subprocess.call(cmd)
+    df = pd.read_csv('Channel_attribution.csv')
+    df = df.fillna(0)
+    cols = df.columns
+
+    for col in cols:
+        df[col] = df[col].astype(int).astype(str)
+        df[col] = df[col].map(lambda x : str(x)[:-2] if '.' in x else str(x))
+
+    df['Path'] = ''
+    for i in df.index:
+        for x in cols:
+            df.at[i, 'Path'] = df.at[i, 'Path'] + df.at[i, x] + ' '
+
+    df['Path'] = df['Path'].map(lambda x: x.split('21')[0])
+    df['Conversion'] = 1
+    df['Conversion_value'] = df['Conversion'].apply(lambda x: x * abs(np.random.normal(20, 5, 1)[0]))
+
+    df = df[['Path', 'Conversion', 'Conversion_value']]
+    
+    df['Path'] = df['Path'].apply(lambda x: x.replace(' ', ' > ')[:-2])
+
+    df = df.groupby('Path').sum().reset_index()
+
+    df.to_csv('Paths.csv', index = False)
+    
+    
+def run_r_script():
+
+    path2script = "Markov Chain Attribution Modeling.R"
+
+    cmd = ['Rscript', path2script]
+
+    subprocess.call(cmd)
+
+    
+if __name__ == "__main__":
+    process_data()
+    run_r_script()
